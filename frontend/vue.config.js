4cbf7ad91d7e4ff564e4e655de6f0a2a44de1d0c
Matthias Schwebler <m.schwebler@sportradar.com>
2021-02-21 16:04:24 +0100

    Migrates frontend to typescript; Adds vue router.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    transpileDependencies: [
+        'vuetify'
+    ],
+    configureWebpack: {
+        resolve: {
+            extensions: ['.js', '.ts', '.vue'],
+        }
+    },
 }
