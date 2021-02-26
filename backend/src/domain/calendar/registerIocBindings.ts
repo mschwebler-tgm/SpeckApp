4c7a039da4081fd9a721265a7e9987d843f58760
Matthias Schwebler <m.schwebler@sportradar.com>
2021-02-26 22:02:46 +0100

    Adds local dynamo db setup.

--- backend/src/domain/calendar/registerIocBindings.ts
+++ backend/src/domain/calendar/registerIocBindings.ts
@@ -6,14 +6,29 @@ import DynamoCalendarRepository from "./repositories/dynamodb/DynamoCalendarRepo
 import {DynamoDB} from "aws-sdk";
 import IUserRepository from "@calendar/repositories/IUserRepository";
 import DynamoUserRepository from "@calendar/repositories/dynamodb/DynamoUserRepository";
+import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 
 export const registerCalendarBindings = (iocContainer: Container) => {
     iocContainer.bind<CalendarService>(iocBindings.CalendarService).to(CalendarService);
     iocContainer.bind<ICalendarRepository>(iocBindings.CalendarRepositoryFactory).toFactory<DynamoCalendarRepository>(() => {
-        return () => new DynamoCalendarRepository(new DynamoDB.DocumentClient({region: process.env.REGION}), process.env.CALENDAR_DYNAMO_TABLE_NAME)
+        return () => new DynamoCalendarRepository(new DynamoDB.DocumentClient(getDynamoOptions()), process.env.CALENDAR_DYNAMO_TABLE_NAME)
     });
     iocContainer.bind<IUserRepository>(iocBindings.UserRepositoryFactory).toFactory<DynamoUserRepository>(() => {
-        return () => new DynamoUserRepository(new DynamoDB.DocumentClient({region: process.env.REGION}), process.env.CALENDAR_DYNAMO_TABLE_NAME)
+        return () => new DynamoUserRepository(new DynamoDB.DocumentClient(getDynamoOptions()), process.env.CALENDAR_DYNAMO_TABLE_NAME)
     });
+}
+
+
+function getDynamoOptions() {
+    const options: DocumentClient.DocumentClientOptions & DynamoDB.Types.ClientConfiguration = {
+        region: process.env.REGION,
+    };
+    if (process.env.IS_LOCAL) {
+        options.endpoint = 'http://localhost:8000';
+        options.accessKeyId = 'xxxxxx';
+        options.secretAccessKey = 'xxxxxx';
+    }
+    console.log(options)
+    return options;
 }
