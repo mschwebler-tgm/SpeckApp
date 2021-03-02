6b681a77660894896b987adc73990b11c58b0794
Matthias Schwebler <m.schwebler@sportradar.com>
2021-03-02 17:42:22 +0100

    Removes unused handler.

--- backend/src/handler.ts
+++
@@ -1,8 +0,0 @@
-import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from "aws-lambda";
-
-export const hello: APIGatewayProxyHandler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return {
-        statusCode: 200,
-        body: 'Hello world',
-    };
-};
